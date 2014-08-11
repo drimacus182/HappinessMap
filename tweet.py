b850776d4225b061df9c70a60d3138ddbf8d85bd
drimacus182 <drimacus182@gmail.com>
2014-08-11 18:41:17 +0300

    streaming twits

--- tweet.py
+++ tweet.py
@@ -1,23 +1,32 @@
-def funct():
-    from TwitterAPI import TwitterAPI
+from TwitterAPI import TwitterAPI
+
+def registerApi():
     api = TwitterAPI("BGMUpePgUoFU13l3NOd26DVUD",
                      "pmiwqlftwhj6tPEDBzRrxvObSw3E8Kt29PIUo0XjGW60jtpOZO",
                      "2719912512-l6a59JAHMpbs8s5t72BHLJqL19sCvUluBw9Ewhh",
                      "1QNsRQKJ6eRTxY9x8HQ3RcdwNd48IF4ywk0MWFI0ILqG7")
+    return api
 
-    #Tweet something:
-    # r = api.request('statuses/update', {'status':'This is a tweet!'})
-    # print r.status_code
-
-    #Get some tweets:
-    r = api.request('search/tweets', {'q':'pizza'})
-    for item in r.get_iterator():
-        print item
+def registerTweetCallback(callback):
+    api = registerApi()
 
     #Stream tweets from New York City:
     r = api.request('statuses/filter', {'locations':'-74,40,-73,41'})
     for item in r.get_iterator():
-        print item
+        callback(item)
+
+#TODO Insert handling code here
+def callback(item):
+    print item
+    return
 
 if __name__ == "__main__":
-    funct()
+    registerTweetCallback(callback)
+
+##### Trash here
+
+
+    #Get some tweets:
+    # r = api.request('search/tweets', {'q':'pizza'})
+    # for item in r.get_iterator():
+    #     print item
